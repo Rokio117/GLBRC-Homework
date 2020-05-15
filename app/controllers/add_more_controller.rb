16c7b912528a8e97b6a1113aa4e582cf45b3dbae
Nick Hillary <hillan02@gmail.com>
2020-05-15 11:58:24 -0500

    finished styles

--- app/controllers/add_more_controller.rb
+++ app/controllers/add_more_controller.rb
@@ -4,15 +4,15 @@ class AddMoreController < ApplicationController
 
   def addApps
     
-    params.each do |id|
-      $apps.map { |app| $defaultApps.push(app) if app.id == id }
+    # params.each do |id|
+    #   $apps.map { |app| $defaultApps.push(app) if app.id == id }
       
-    end
-    redirect_to '/home'
+    # end
+    # redirect_to '/home'
   end
 
   def cancel
-    puts 'PAODUHFBGPOIAUHRPAIOUHRGPIAOSUFGHPIOUASDFHPAIOUSDHFPAISUDFHBPAIOUSGBPAIURGBPAIUSDASDFHAPISDHUFPASIDOFHAPDHF'
+   
     redirect_to '/home'
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,14 +4,14 @@ class HomeController < ApplicationController
 
   def delete
    
-    @removeApp = []
-   $apps.map {|app| if app.name == params[:name]
-    @removeApp.push(app)
-  end }
-   $nonDefaultApps.push(@removeApp)
-   $defaultApps.delete(@removeApp)
-   puts @removeApp
-   redirect_to '/home'
+  #   @removeApp = []
+  #  $apps.map {|app| if app.name == params[:name]
+  #   @removeApp.push(app)
+  # end }
+  #  $nonDefaultApps.push(@removeApp)
+  #  $defaultApps.delete(@removeApp)
+  #  puts @removeApp
+  #  redirect_to '/home'
   end
 
 
