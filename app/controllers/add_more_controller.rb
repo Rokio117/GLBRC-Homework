51e17dd5ea1dca32d596a805cb79293cc26eb0e8
Nick Hillary <hillan02@gmail.com>
2020-05-14 20:42:33 -0500

    added grid display to add apps page

--- app/controllers/add_more_controller.rb
+++ app/controllers/add_more_controller.rb
@@ -1,4 +1,8 @@
 class AddMoreController < ApplicationController
   def add_more
   end
+
+  def cancel
+    redirect_to '/home'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'home' => 'home#home'
   get '/login/submit' => 'login#submit'
   get '/add_more' => 'add_more#add_more'
+
+  get '/back' => 'add_more#cancel'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
